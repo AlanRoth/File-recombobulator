57e2fedf3832bac2217f036b77a40cc6bf292b3c
Alan Roth <alan.roth@payara.fish>
2019-02-12 17:25:02 +0000

    added more to file manager

--- src/main/java/org/alan/roth/filemanipulator/UserIO.java
+++ src/main/java/org/alan/roth/filemanipulator/UserIO.java
@@ -3,13 +3,22 @@
  */
 package org.alan.roth.filemanipulator;
 
+import java.util.Scanner;
+import utility.FileManager;
+
 /**
  *
  * @author asroth
  */
-public class UserIO {
+public class UserIO {  
+    private Scanner reader;
     
     public static void main(String[] args) {
+        FileManager fm = new FileManager();
+        System.out.println(fm.getFileNumber());
+    }
+    
+    public void selectionScreen(){
         
     }
     
--- src/main/java/utility/FileManager.java
+++ src/main/java/utility/FileManager.java
@@ -14,14 +14,19 @@ public class FileManager {
     private String filePath1 = "../Resources/file1";
     private String filePath2 = "../Resources/file2";
     private String filePath3 = "../Resources/file3";
-    private ArrayList<File> fileBuffer;
+    private ArrayList<String> filePathBuffer = new ArrayList<String>();
+    private ArrayList<File> fileBuffer = new ArrayList<File>();
     
     public FileManager(){
+        filePathBuffer.add(filePath1);
+        filePathBuffer.add(filePath2);
+        filePathBuffer.add(filePath3);
         
-    }
+        addFiles();
+    }         
     
-    public FileManager(String filePath){
-        this.filePath1 = filePath;
+    public FileManager(String filePath1){
+        this.filePath1 = filePath1;
     }
     
     public FileManager(String filePath1, String filePath2){
@@ -29,23 +34,41 @@ public class FileManager {
         this.filePath2 = filePath2;
     }
     
-    public FileManager(String filePath1, String filePath2, String filePath3){
+    public FileManager(String filePath1, String filePath2, String filePath3){    
         this.filePath1 = filePath1;
         this.filePath2 = filePath2;
         this.filePath3 = filePath3;
-    }
-      
+    }  
+    
     public FileManager(String...filePath){
-        for(String path : filePath){
-            
-            File newFile = new File(path);
-            
-            if(newFile.exists()){
-                fileBuffer.add(newFile);
-            }
-            
-        }
+        filePathBuffer = new ArrayList<>();
+        fileBuffer = new ArrayList<>();
         
+        for(String path : filePath){           
+            if(path != null || !(path.trim().isEmpty())){
+                filePathBuffer.add(path);
+            }        
+        }  
+        
+        addFiles();
+    }   
+    
+    private void addFiles(){
+        for(String path : filePathBuffer){
+            File file = new File(path);
+            if(file.exists()){
+                fileBuffer.add(file);
+            }else{
+                System.out.println("Couldn't find file at: " + path);
+            }
+        }
     }
     
+    public int getFileNumber(){
+        return fileBuffer.size();
+    }
+    
+    public ArrayList<File> getFileBuffer(){
+        return fileBuffer;
+    }  
 }
